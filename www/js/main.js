2cd3d42fde815e46a7c1666c8218ef7d45e92c74
Fabricio C Zuardi <fabricio@fabricio.org>
2013-10-22 10:40:06 -0200

    press S to select a photo

--- www/js/main.js
+++ www/js/main.js
@@ -16,27 +16,36 @@ require(
         var listData,
             photoList,
             currentIndex = 0,
+            photoCountStart = 500,
             newAttributes = [];
-        text.get('../data/metadata/data_page_001.json', function(data){
+        text.get('../data/metadata/data_page_006.json', function(data){
             listData = JSON.parse(data);
             photoList = listData.photos.photo;
             document.getElementById('current').setAttribute('src', photoList[currentIndex].url_c);
             document.getElementById('previous').setAttribute('src', '');
         });
+        function getLocalPictureURL(url){
+            return '../data/pictures/' + url.substring( url.lastIndexOf('/')+1, url.length );
+        }
         function updatePhotoURLs(){
-            document.getElementById('current').setAttribute('src', photoList[currentIndex].url_c);
+            document.getElementById('current').setAttribute('src', getLocalPictureURL(photoList[currentIndex].url_c));
             if (photoList[currentIndex-1] !== undefined){
-                document.getElementById('previous').setAttribute('src', photoList[currentIndex-1].url_c);
+                document.getElementById('previous').setAttribute('src', getLocalPictureURL(photoList[currentIndex-1].url_c));
             } else{
                 document.getElementById('previous').setAttribute('src', '');
             }
         }
         function updateAttributes(){
+            if (newAttributes[currentIndex] === undefined){
+                document.getElementById('current').dataset.selected = false;
+                return false;
+            }
             console.log('updateAttributes', newAttributes[currentIndex]);
             if (newAttributes[currentIndex] !== undefined){
                 document.getElementById('current').style.width = newAttributes[currentIndex].width;
                 document.getElementById('current').style.top = newAttributes[currentIndex].top;
                 document.getElementById('current').style.left = newAttributes[currentIndex].left;
+                document.getElementById('current').dataset.selected = (newAttributes[currentIndex].selected === 'true');
             }
             if (newAttributes[currentIndex - 1] !== undefined){
                 document.getElementById('previous').style.width = newAttributes[currentIndex - 1].width;
@@ -72,46 +81,50 @@ require(
                     break;
                 case 0:
                     switch (event.charCode){
+                        case 115:
+                            // s
+                            currentImg.dataset.selected = (currentImg.dataset.selected === 'true') ? false : true;
+                            break;
                         case 93:
-                            //]
+                            // ]
                             increment = 10;
                             currentImg.style.width = (parseInt(currentImg.style.width, 10) + increment) + 'px';
                             currentImg.style.left = (parseInt(currentImg.style.left, 10) - increment/2) + 'px';
                             break;
                         case 91:
-                            //[
+                            // [
                             increment = -10;
                             currentImg.style.width = (parseInt(currentImg.style.width, 10) + increment) + 'px';
                             currentImg.style.left = (parseInt(currentImg.style.left, 10) - increment/2) + 'px';
                             break;
                         case 61:
-                            //+
+                            // +
                             increment = 40;
                             currentImg.style.width = (parseInt(currentImg.style.width, 10) + increment) + 'px';
                             currentImg.style.left = (parseInt(currentImg.style.left, 10) - increment/2) + 'px';
                             break;
                         case 45:
-                            //-
+                            // -
                             increment = -40;
                             currentImg.style.width = (parseInt(currentImg.style.width, 10) + increment) + 'px';
                             currentImg.style.left = (parseInt(currentImg.style.left, 10) - increment/2) + 'px';
                             break;
                         case 46:
-                            //>
+                            // >
                             console.log('next');
                             currentIndex += 1;
                             updatePhotoURLs();
                             updateAttributes();
                             break;
                         case 44:
-                            //<
+                            // <
                             currentIndex -= 1;
                             updatePhotoURLs();
                             updateAttributes();
                             console.log('previous');
                             break;
                         case 99:
-                            //c (for copy/paste)
+                            // c (for copy/paste)
                             return;
                             break;
                     }
@@ -121,7 +134,8 @@ require(
                 'height': currentImg.clientHeight,
                 'left': currentImg.style.left,
                 'top': currentImg.style.top,
-                'filename': currentImg.src
+                'filename': currentImg.src,
+                'selected': currentImg.dataset.selected
             };
             output = 'convert -size 1000x1000 xc:white data/moved/base.jpg\n';
             newAttributes.forEach(function(image, index){
@@ -130,17 +144,18 @@ require(
                     l = parseInt(image.left, 10),
                     t = parseInt(image.top, 10),
                     filename = image.filename.substring( image.filename.lastIndexOf('/')+1, image.filename.length ),
-                    paddedIndex = String('000' + index).slice(-3);
+                    paddedIndex = String('000' + (index + photoCountStart)).slice(-3);
 
                 l = (l>=0)?('+' + l):(l);
                 t = (t>=0)?('+' + t):(t);
 
-
-                output += 'composite -size 1000x1000 -geometry '+
-                            w +'x'+ h + l + t +' data/pictures/'+ filename +
-                            ' data/moved/base.jpg data/moved/' + paddedIndex + '.jpg\n';
+                if (image.selected === 'true'){
+                    output += 'composite -size 1000x1000 -geometry '+
+                                w +'x'+ h + l + t +' data/pictures/'+ filename +
+                                ' data/moved/base.jpg data/front/' + paddedIndex + '.jpg\n';
+                }
             });
-            output += 'convert -loop 1 data/moved/0*.jpg facelapse.gif\n';
+            output += 'convert -loop 1 data/front/*.jpg facelapse_front.gif\n';
             // document.getElementById('output').innerHTML = JSON.stringify(newAttributes, null, '  ');
             document.getElementById('output').innerHTML = output;
         });
